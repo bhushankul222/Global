4a19c4971bb48dd100d5442012ce6e1f3238a664
Bhushan Kalsarpe <bhushankul222@gmail.com>
2021-09-14 13:56:31 +0530

    my app

---
+++ Animal.java
@@ -0,0 +1,17 @@
+class Animal{
+
+String colour;
+int age;
+
+public static void main(String[] args){
+
+Animal rocky = new Animal();
+rocky.colour= "black";
+rocky.age= 10;
+
+System.out.println(rocky.colour+" "+rocky.age);
+}
+
+
+
+}
